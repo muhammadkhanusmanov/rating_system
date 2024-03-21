4e8fc031d4bd12e49db2dbcb34a831285ba7bcd9
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-21 18:23:14 +0500

    user staff

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class StaffUser(models.Model):
+    staff_choice = [
+        ('TEACHER','teacher'),
+        ('STUDENT','student'),
+        ('ADMIN','admin')
+    ]
+    staff = models.CharField(max_length=8,choices=staff_choice, default='TEACHER')
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='staff')
