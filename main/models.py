551af325fee75832411aad27a370c5abee44e396
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-21 23:53:56 +0500

    teacher and subject

--- main/models.py
+++ main/models.py
@@ -10,3 +10,14 @@ class StaffUser(models.Model):
     ]
     staff = models.CharField(max_length=8,choices=staff_choice, default='TEACHER')
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='staff')
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    full_name = models.CharField(max_length=90)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teacher')
+    subjects = models.ManyToManyField(Subject,related_name='subject')
+
