e5523e90a62eb36cd841f1196556e320a944f585
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-22 15:32:34 +0500

    media url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import Signin
+from main.views import Signin,GetUser
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/login/', Signin.as_view())
-]
+    path('user/login/', Signin.as_view()),
+    path('student/subjects/',GetUser.as_view())
+] + static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
