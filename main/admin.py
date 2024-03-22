a6b92203e555a9e73ec1cd2626c09956f468836f
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-22 15:31:44 +0500

    register

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import (
-    StaffUser,Subjectss
+    StaffUser,Topic,Teachers
 )
 
 admin.site.register([
-    StaffUser,Subjectss          
+    StaffUser,Teachers,Topic       
 ])
