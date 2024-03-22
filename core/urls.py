eb8ec1328567ca59d8e25a88ac294ac1da0dcfff
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-22 15:52:55 +0500

    get teachers url

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/login/', Signin.as_view()),
-    path('student/subjects/',GetUser.as_view())
+    path('student/subjects/',GetUser.as_view()),
+    path('teachers/<str:id>',GetUser.as_view())
 ] + static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
