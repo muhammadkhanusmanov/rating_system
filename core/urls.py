2aedc23899a658fe5459c35013950d94c5a8275e
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-22 10:44:00 +0500

    login url

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from ..main.views import Signin
+from main.views import Signin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
