391aeea38a3f81310ab7b97a40823d8ebf2b8480
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-22 15:32:08 +0500

    teacher model

--- main/models.py
+++ main/models.py
@@ -11,11 +11,15 @@ class StaffUser(models.Model):
     staff = models.CharField(max_length=8,choices=staff_choice, default='TEACHER')
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='staff')
 
+class Teachers(models.Model):
+    image = models.ImageField(upload_to='teacher_image/')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Subjectss(models.Model):
+class Topic(models.Model):
     name = models.CharField(max_length=30)
     full_name = models.CharField(max_length=90)
-    teachers = models.ManyToManyField(User)
+    img = models.ImageField(upload_to='subject_images/')
+    teachers = models.ManyToManyField(Teachers)
     student = models.ManyToManyField(User, related_name='subject')
 
 
