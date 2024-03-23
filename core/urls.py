2ba7c3ca8e74411bf2baf75d5e16c5307ce360c8
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-23 17:09:57 +0500

    put mark

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import Signin,GetUser
+from main.views import Signin,GetUser,MarksView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/login/', Signin.as_view()),
     path('student/subjects/',GetUser.as_view()),
-    path('teachers/<str:id>',GetUser.as_view())
+    path('teachers/<str:id>',GetUser.as_view()),
+    path('putmark/',MarksView.as_view())
 ] + static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
