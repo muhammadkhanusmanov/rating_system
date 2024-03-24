70ba9e351819c9de4842032b8057cfe235a7eeee
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-24 10:49:55 +0500

    add allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5k69zj2)7f9fulhdcj&us8a)b3#uv@tc6w101wws!8c(cm251j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
